c0491e91b5a5c82b7545ceb2c484489b454eefe7
Pedro Santayana <pedrosantayana@gmail.com>
2022-06-24 23:32:52 -0300

    fix responsive

--- detalhes.js
+++ detalhes.js
@@ -10,10 +10,10 @@ if(data) {
   data.then(film => {
     console.log(film);
     let card = document.createElement("div");
-    card.className = "card p-3 border-3 rounded-3";
+    card.className = "card";
     card.innerHTML = `
       <div class="row g-0">
-        <div class="col-md-4">
+        <div class="col-md-4 p-3">
           <img src="https://image.tmdb.org/t/p/original${film.poster_path}" class="img-fluid rounded-start" alt="Poster de filme">
         </div>
         <div class="col-md-8">
@@ -21,8 +21,8 @@ if(data) {
             <h5 class="card-title">${film.title}</h5>
             <p class="card-text">${film.overview}</p>
             <p class="card-text"><small class="text-muted">${film.release_date.split("-").reverse().join("/")} - ${film.vote_average}/10</small></p>
-            <div id="genres" class="d-flex"></div>
-            <div class="d-flex justify-content-end">
+            <div id="genres" class="container-fluid"></div>
+            <div class="d-flex h-100 bg-primary justify-content-end">
               <a href="${film.homepage}" class="btn btn-dark">Site do filme</a>
             </div>
           </div>
--- modules/components.mjs
+++ modules/components.mjs
@@ -4,6 +4,12 @@
  * @returns 
  */
 export function cardFilm(film) {
+  let star = document.createElement("i");
+  star.className = "fa-solid fa-star";
+
+  let half_star = document.createElement("i");
+  half_star.className = "fa-solid fa-star-half-stroke";
+
   let card = document.createElement("div");
   card.className = "p-3";
   card.innerHTML = `
@@ -15,14 +21,37 @@ export function cardFilm(film) {
       <div class="col-md-8">
         <div class="card-body">
           <h5 class="card-title">${film.title}</h5>
-          <div class="d-flex justify-content-between mt-auto">
-            <small class="text-muted">${film.release_date.split("-").reverse().join("/")} - ${film.vote_average}/10</small>
-            <a href="detalhes.html?id=${film.id}" class="btn btn-dark">Detalhes</a>
+          <small class="text-muted fw-bold">${film.release_date.split("-").reverse().join("/")}</small>
+          <div id="votes"></div>
+          <div class="d-flex justify-content-end">
+            <a href="detalhes.html?id=${film.id}" class="btn btn btn-dark">Detalhes</a>
           </div>
         </div>
       </div>
       </div>
     </div>
     `;
+
+  let a = Math.round(film.vote_average * 2);
+
+  for (let i = 0; i < a/4; i++) {
+    card.querySelector("#votes").appendChild(star.cloneNode());
+  }
+  if(a%2==1) card.querySelector("#votes").appendChild(half_star);
   return card;
+}
+
+function createStars(votes) {
+  let star = document.createElement("i");
+  star.className = "fa-solid fa-star";
+
+  let half_star = document.createElement("i");
+  half_star.className = "fa-solid fa-star-half-stroke";
+
+  let a = Math.round(film.avarege_vote * 2);
+  let arr = [];
+  for (let i = 0; i < film.avarege_vote%2; index++) {
+    arr.push(star);
+  }
+
 }
